7295716b4c25ef2dbfb35556ee48ce75828bcfa4
Denis Benato <benato.denis96@gmail.com>
2016-08-01 14:36:45 +0200

    Fix del progetto

--- src/pizza/Pizza.java
+++ src/pizza/Pizza.java
@@ -18,7 +18,11 @@
 package pizza;
 
 import java.io.IOException;
+import java.lang.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jibble.pircbot.*;
+import java.util.regex.*;
 
 /**
  * The main bot class
@@ -36,27 +40,61 @@ public class Pizza extends PircBot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("Pizza  Copyright (C) 2016  pierotofy.it community\n"
+                + "This program comes with ABSOLUTELY NO WARRANTY;\n"
+                + "This is free software, and you are welcome to redistribute it\n"
+                + "under certain conditions.\n\n");
+        
+        String botName = "", botServer = "", botChannel = "";
         try {
-            String botName = args[0];
-            String botServer = args[1];
-            String botChannel = args[2];
-        } catch (java.lang.IndexOutOfBoundsException ex) {
-            System.out.print("Usage: java -jar Pizza.jar BotName Server:Port #Channel");
+            botName = args[0];
+            botServer = args[1];
+            botChannel = args[2];
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Utilizzo: java -jar Pizza.jar BotName server[:port] #channel");
+            System.exit(-1);
         }
         
-        // Now start our bot up.
+        // Prova a estrarre server e porta
+        String server = "irc.freenode.net", port = "6667";
+        Pattern serverRegex = Pattern.compile("(\\w+)\\.(\\w+)\\.(\\w+)(\\:(\\d+))?");
+        Matcher serverMatcher = serverRegex.matcher(botServer);
+        int portSeparator = botServer.indexOf(":");
+        if (!serverMatcher.matches()) {
+            System.out.println("Il server specificato non e' valido");
+            System.exit(-1);
+        } else if (portSeparator <= -1) {
+            botServer = botServer + ":6667";
+        } else if (portSeparator == 0) {
+            System.out.println("Il nome del server e' errato");
+            System.exit(-1);
+        }
+        server = botServer.substring(0, portSeparator);
+        port = botServer.substring(portSeparator+1, botServer.length());
+        
+        // Controlla il nome del canale
+        Pattern channelRegex = Pattern.compile("\\#(([\\w]?)([\\d]?)([\\-]?)([\\_]?)([\\.]?))+");
+        Matcher channelMatcher = channelRegex.matcher(botChannel);
+        if (!channelMatcher.matches()) {
+            System.out.println("Il nome del canale specificato non e' valido");
+            System.exit(-1);
+        }
+        
+        // Inizializza il bot
         Pizza bot = new Pizza(args[0]);
         
-        // Enable debugging output.
-        bot.setVerbose(true);
+        // Abilita l'output di debug
+        // bot.setVerbose(true);
         
         try {
-            // Connect to the IRC server.
-            bot.connect("irc.pierotofy.it", 6669);
+            // Connetti il bot al server
+            bot.connect(server, Integer.parseInt(port));
 
-            // Join the IRC channel.
-            bot.joinChannel("#pierotofy.it");
+            // E unisciti al canale
+            bot.joinChannel(botChannel);
         } catch (IOException ex) {
+            
+        } catch (IrcException ex) {
             
         }
     }
