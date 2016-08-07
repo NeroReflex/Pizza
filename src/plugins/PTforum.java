301c907ea51945da8c551c42fdcc5317f29b771f
Gianluca Nitti <gianluca.nitti96@gmail.com>
2016-08-07 15:43:41 +0200

    Versione provvisoria che stampa l'elenco degli ultimi topic ogni minuto.

--- src/plugins/PTforum.java
+++ src/plugins/PTforum.java
@@ -1,5 +1,14 @@
 package plugins;
 
+import java.net.URL;
+import java.io.InputStream;
+import java.io.IOException;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonValue;
+import javax.json.JsonObject;
+import pizza.Message;
+
 /**
  * Plugin per notificare nuovi messaggi sul forum di pierotofy.it
  *
@@ -7,16 +16,24 @@ package plugins;
  */
 public final class PTforum extends pizza.Trancio{
   String chan = "#pierotofy.it";
-  int interval = 20000; //TODO: rendere configurabili
+  String apiEndpoint = "http://www.pierotofy.it/pages/extras/forum/api/last_topics.php";
+  int interval = 60000; //TODO: rendere configurabili
 
   protected final void onPoll(){
-    while(true){
-      sendMessage(new pizza.Message(chan, "PROVA (viene mandato ogni 20s)"));
+      //sendMessage(new Message(chan, "PROVA (viene mandato ogni 20s)"));
       try{
-        Thread.sleep(interval);
+          InputStream is = new URL(apiEndpoint).openStream();
+          JsonArray topicList = Json.createReader(is).readArray();
+          for(JsonValue v: topicList){
+              JsonObject obj = (JsonObject)v;
+              sendMessage(new Message(chan, obj.getString("subject") + " http://pierotofy.it" + obj.getString("url")));
+          }
+          Thread.sleep(interval);
+      }catch(IOException e){
+          e.printStackTrace();
+          sendMessage(new Message(chan, "Attenzione: impossibile recuperare gli aggiornamenti dal forum."));
       }catch(InterruptedException e){
-        e.printStackTrace();
+          e.printStackTrace();
       }
-    }
   }
 }
