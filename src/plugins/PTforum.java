4fc026355704a7b5e5eb76a24994d1b9695c9337
Gianluca Nitti <gianluca.nitti96@gmail.com>
2016-08-08 14:57:31 +0200

    Plugin notifiche forum: notifica nuovi post in topic esistenti.
    Per ora non notifica nuovi topic (TODO).

--- src/plugins/PTforum.java
+++ src/plugins/PTforum.java
@@ -1,5 +1,7 @@
 package plugins;
 
+import java.util.HashMap;
+import java.util.ArrayList;
 import java.net.URL;
 import java.io.InputStream;
 import java.io.IOException;
@@ -17,16 +19,40 @@ import pizza.Message;
 public final class PTforum extends pizza.Trancio{
   String chan = "#pierotofy.it";
   String apiEndpoint = "http://www.pierotofy.it/pages/extras/forum/api/last_topics.php";
-  int interval = 60000; //TODO: rendere configurabili
+  int interval = 300000; //TODO: rendere configurabili
+
+  HashMap<String, Integer> topics = new HashMap<String, Integer>(); //chiave=url, valore=numero risposte
 
   protected final void onPoll(){
-      //sendMessage(new Message(chan, "PROVA (viene mandato ogni 20s)"));
       try{
           InputStream is = new URL(apiEndpoint).openStream();
           JsonArray topicList = Json.createReader(is).readArray();
           for(JsonValue v: topicList){
               JsonObject obj = (JsonObject)v;
-              sendMessage(new Message(chan, obj.getString("subject") + " http://pierotofy.it" + obj.getString("url")));
+              Integer oldReplies = topics.get(obj.getString("url"));
+              //System.out.println(obj.getString("subject")); //per debug
+              //System.out.println(obj.getInt("replies"));
+              if(oldReplies == null)
+                  topics.put(obj.getString("url"), obj.getInt("replies")); //se non è già in elenco, lo aggiungo
+              else if(obj.getInt("replies") > oldReplies) //Se il numero di risposte è maggiore, allora ci sono stati nuovi post
+                  sendMessage(new Message(chan, "Ci sono nuovi messaggi nel topic \"" + obj.getString("subject") + "\": http://pierotofy.it" + obj.getString("url")));
+          }
+          ArrayList<String> topicsToRemove = new ArrayList<String>();
+          //Pulisce la hashmap dai vecchi topic (che non sono più nella risposta dell'api)
+          for(String url: topics.keySet()){
+              boolean toRemove = true;
+              for(JsonValue v: topicList){
+                  if(((JsonObject)v).getString("url").equals(url)){
+                      toRemove = false;
+                      break;
+                  }
+              }
+              if(toRemove)
+                  topicsToRemove.add(url);
+          }
+          for(String s: topicsToRemove){
+              topics.remove(s);
+              System.out.println("Rimosso vecchio topic " + s); //debug
           }
           Thread.sleep(interval);
       }catch(IOException e){
