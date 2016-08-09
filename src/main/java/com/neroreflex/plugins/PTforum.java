b9f3b415d8c11d083505c5b2642e6b57494e208e
Denis Benato <benato.denis96@gmail.com>
2016-08-09 19:32:17 +0200

    Ora il bot avverte tutti i canali.

--- src/main/java/com/neroreflex/plugins/PTforum.java
+++ src/main/java/com/neroreflex/plugins/PTforum.java
@@ -29,7 +29,7 @@ import com.neroreflex.pizza.*;
  * @author Nitti Gianluca
  */
 public final class PTforum extends Trancio {
-  String chan = "#pierotofy.it";
+
   String apiEndpoint = "http://www.pierotofy.it/pages/extras/forum/api/last_topics.php";
   int interval = 300000; //TODO: rendere configurabili
 
@@ -37,24 +37,32 @@ public final class PTforum extends Trancio {
 
   protected final void onPoll(){
       try{
+          String[] channels = this.getChannels();
           InputStream is = new URL(apiEndpoint).openStream();
           JsonArray topicList = Json.createReader(is).readArray();
           boolean firstFetch = (topics.size() == 0);
           for(JsonValue v: topicList){
-              JsonObject obj = (JsonObject)v;
-              Integer oldReplies = topics.get(obj.getString("url"));
-              //System.out.println(obj.getString("subject")); //per debug
-              //System.out.println(obj.getInt("replies"));
-              if(oldReplies == null){
-                  topics.put(obj.getString("url"), obj.getInt("replies")); //se non è già in elenco, lo aggiungo...
-                  if(!firstFetch)
-                      sendMessage(new Message(chan, "E' stato aperto un nuovo topic nel forum: \"" + obj.getString("subject") + "\": http://pierotofy.it" + obj.getString("url"))); //... e mando il messaggio
-              }
-              else if(obj.getInt("replies") > oldReplies) //Se il numero di risposte è maggiore, allora ci sono stati nuovi post
-                  sendMessage(new Message(chan, "Ci sono nuovi messaggi nel topic \"" + obj.getString("subject") + "\": http://pierotofy.it" + obj.getString("url")));
-          }
+            JsonObject obj = (JsonObject)v;
+            Integer oldReplies = topics.get(obj.getString("url"));
+
+            if(oldReplies == null){
+                // Se non è già in elenco, lo aggiungo...
+                topics.put(obj.getString("url"), obj.getInt("replies"));
+                if(!firstFetch)
+                    for (int i = 0; i < channels.length; i++) {
+                         String chan = channels[i];
+                        sendMessage(new Message(chan, "Ci sono nuovi messaggi nel topic \"" + obj.getString("subject") + "\": http://pierotofy.it" + obj.getString("url")));
+                    }
+            } else if(obj.getInt("replies") > oldReplies) {
+                // Se il numero di risposte è maggiore, allora ci sono stati nuovi post
+                for (int i = 0; i < channels.length; i++) {
+                        String chan = channels[i];
+                        sendMessage(new Message(chan, "Ci sono nuovi messaggi nel topic \"" + obj.getString("subject") + "\": http://pierotofy.it" + obj.getString("url")));
+                    }
+                }
+            }
           ArrayList<String> topicsToRemove = new ArrayList<>();
-          //Pulisce la hashmap dai vecchi topic (che non sono più nella risposta dell'api)
+          // Pulisce la hashmap dai vecchi topic (che non sono più nella risposta dell'api)
           for(String url: topics.keySet()){
               boolean toRemove = true;
               for(JsonValue v: topicList){
@@ -66,14 +74,13 @@ public final class PTforum extends Trancio {
               if(toRemove)
                   topicsToRemove.add(url);
           }
-          for(String s: topicsToRemove){
+          for(String s: topicsToRemove)
               topics.remove(s);
-              //System.out.println("Rimosso vecchio topic " + s); //debug
-          }
+          
           Thread.sleep(interval);
       }catch(IOException e){
           e.printStackTrace();
-          sendMessage(new Message(chan, "Attenzione: impossibile recuperare gli aggiornamenti dal forum."));
+          //sendMessage(new Message(chan, "Attenzione: impossibile recuperare gli aggiornamenti dal forum."));
       }catch(InterruptedException e){
           e.printStackTrace();
       }
